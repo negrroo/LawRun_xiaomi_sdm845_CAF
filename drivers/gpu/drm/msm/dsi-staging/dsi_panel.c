e7173da62659374b1dd23607adaf653b4bc30149
negrroo <mohammedaelnaggar1@gmail.com>
2020-11-07 18:27:03 +0200

    drivers: gpu: Allow wider framerate range
    -Improve commit https://github.com/negrroo/LawRun_xiaomi_sdm845_CAF/commit/72d9a76985d56989582be04411cf6cad24ba6b19
    -Remove useless lines
    -Adding 70hz support
    
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1166,16 +1166,6 @@ static int dsi_panel_parse_timing(struct device *parent,
 	}
 
 	mode->clk_rate_hz = !rc ? tmp64 : 0;
-	if (tmp64 == 1100000000 || tmp64 == 1103000000) {
-		if (framerate_override == 4)
-			mode->clk_rate_hz = 1375000000;
-		else if (framerate_override == 3)
-			mode->clk_rate_hz = 1320000000;
-		else if (framerate_override == 2)
-			mode->clk_rate_hz = 1265000000;
-		else if (framerate_override == 1)
-			mode->clk_rate_hz = 1210000000;
-	}
 	display_mode->priv_info->clk_rate_hz = mode->clk_rate_hz;
 
 	rc = dsi_panel_parse(of_node, fw_entry,
@@ -1202,8 +1192,10 @@ static int dsi_panel_parse_timing(struct device *parent,
 	}
 
 	if (mode->refresh_rate == 60) {
-		if (framerate_override == 7)
+		if (framerate_override == 8)
 			mode->refresh_rate = 71;
+		else if (framerate_override == 7)
+			mode->refresh_rate = 70;
 		else if (framerate_override == 6)
 			mode->refresh_rate = 69;
 		else if (framerate_override == 5)
