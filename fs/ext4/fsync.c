a7ebac9d9dd8b25f7bedef2e8937effb23282c02
stic-server-open <1138705738@qq.com>
2021-02-25 17:36:05 +0800

    fs: ext4: Import Xiaomi AFSYNC Mode
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- fs/ext4/fsync.c
+++ fs/ext4/fsync.c
@@ -27,11 +27,15 @@
 #include <linux/sched.h>
 #include <linux/writeback.h>
 #include <linux/blkdev.h>
+#include <linux/cred.h>
+#include <linux/uidgid.h>
 
 #include "ext4.h"
 #include "ext4_jbd2.h"
 
 #include <trace/events/ext4.h>
+
+#define AID_SYSTEM 1000 /* system server */
 
 /*
  * If we're not journaling and this is a just-created file, we have to
@@ -145,6 +149,20 @@ int ext4_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 	if (journal->j_flags & JBD2_BARRIER &&
 	    !jbd2_trans_will_send_data_barrier(journal, commit_tid))
 		needs_barrier = true;
+	if (test_opt(inode->i_sb, ASYNC_FSYNC)) {
+		atomic_inc(&EXT4_SB(inode->i_sb)->s_total_fsync);
+
+		if (!uid_eq(GLOBAL_ROOT_UID, current_fsuid()) &&
+			  !(in_group_p(make_kgid(current_user_ns(), AID_SYSTEM)))) {
+			atomic_inc(&EXT4_SB(inode->i_sb)->s_async_fsync);
+
+			if (jbd2_transaction_need_wait(journal, commit_tid))
+				jbd2_log_start_commit(journal, commit_tid);
+
+			goto out;
+		}
+	}
+
 	ret = jbd2_complete_transaction(journal, commit_tid);
 	if (needs_barrier) {
 	issue_flush:
