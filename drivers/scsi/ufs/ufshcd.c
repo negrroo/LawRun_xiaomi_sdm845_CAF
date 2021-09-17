e29b1adc83b65a95429fce5c87efce6850229000
negrroo <mohammedaelnaggar1@gmail.com>
2021-09-17 03:32:59 +0200

    Revert "scsi: ufs: Fix a bug in ufshcd_system_resume()"
    This reverts commit d7c56110f47102689dd151fb703fb23eb293ecb8.
    We patch wrong lines
    
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9696,15 +9696,8 @@ out:
 	trace_ufshcd_system_suspend(dev_name(hba->dev), ret,
 		ktime_to_us(ktime_sub(ktime_get(), start)),
 		hba->curr_dev_pwr_mode, hba->uic_link_state);
-	if (!ret) {
+	if (!ret)
 		hba->is_sys_suspended = true;
-
-		if (pm_runtime_suspended(hba->dev)) {
-			pm_runtime_disable(hba->dev);
-			pm_runtime_set_active(hba->dev);
-			pm_runtime_enable(hba->dev);
-		}
-	}
 	return ret;
 }
 EXPORT_SYMBOL(ufshcd_system_suspend);
@@ -9725,6 +9718,10 @@ int ufshcd_system_resume(struct ufs_hba *hba)
 		return -EINVAL;
 
 	if (!hba->is_powered || pm_runtime_suspended(hba->dev))
+		/*
+		 * Let the runtime resume take care of resuming
+		 * if runtime suspended.
+		 */
 		goto out;
 	else
 		ret = ufshcd_resume(hba, UFS_SYSTEM_PM);
